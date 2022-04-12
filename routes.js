83b63dcf5c180534a67c7eb4a334007655dc0981
Maya Kano <mayakanoweb@gmail.com>
2022-04-12 15:01:03 -0400

    final changes to server.js

--- routes.js
+++ routes.js
@@ -1,5 +1,15 @@
 const shortid = require("short-id");
-function routes(app, db) {
+const IPFS = require("ipfs-api");
+const ipfs = new IPFS({
+  host: "ipfs.infura.io",
+  port: 5001,
+  protocol: "https",
+});
+
+function routes(app, dbe, lms, accounts) {
+  let db = dbe.collection("music-users");
+  let music = dbe.collection("music-store");
+
   app.post("/register", (req, res) => {
     let email = req.body.email;
     let idd = shortid.generate();
@@ -18,6 +28,7 @@ function routes(app, db) {
       res.status(400).json({ status: "Failed", reason: "wrong input" });
     }
   });
+
   app.post("/login", (req, res) => {
     let email = req.body.email;
     if (email) {
@@ -32,20 +43,61 @@ function routes(app, db) {
       res.status(400).json({ status: "Failed", reason: "wrong input" });
     }
   });
-  app.post("/upload", (req, res) => {
+  app.post("/upload", async (req, res) => {
     let buffer = req.body.buffer;
     let name = req.body.name;
     let title = req.body.title;
+    let id = shortid.generate() + shortid.generate();
     if (buffer && title) {
+      //console.log(ipfs)
+      let ipfsHash = await ipfs.add(buffer || Buffer.alloc(8));
+      let hash = ipfsHash[0].hash;
+      lms
+        .sendIPFS(id, hash, { from: accounts[0] })
+        .then((_hash, _address) => {
+          music.insertOne({ id, hash, title, name });
+          res.json({ status: "success", id });
+        })
+        .catch((err) => {
+          res
+            .status(500)
+            .json({ status: "Failed", reason: "Upload error occured" });
+        });
     } else {
       res.status(400).json({ status: "Failed", reason: "wrong input" });
     }
   });
-  app.get("/access/:email/:id", (req, res) => {
+  app.get("/access/:email", (req, res) => {
+    if (req.params.email) {
+      db.findOne({ email: req.body.email }, (err, doc) => {
+        if (doc) {
+          let data = music.find().toArray();
+          res.json({ status: "success", data });
+        }
+      });
+    } else {
+      res.status(400).json({ status: "Failed", reason: "wrong input" });
+    }
+  });
+  app.get("/access/:email/:id", async (req, res) => {
+    let id = req.params.id;
     if (req.params.id && req.params.email) {
+      db.findOne({ email: req.body.email }, (err, doc) => {
+        if (doc) {
+          lms.getHash(id, { from: accounts[0] }).then(async (hash) => {
+            console.log(hash);
+            let data = await ipfs.files.get(hash);
+            console.log(data);
+            res.json({ status: "success", data: data.content });
+          });
+        } else {
+          res.status(400).json({ status: "Failed", reason: "wrong input" });
+        }
+      });
     } else {
       res.status(400).json({ status: "Failed", reason: "wrong input" });
     }
   });
 }
+
 module.exports = routes;
